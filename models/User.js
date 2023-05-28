bd4d4c917f4e4ee003726535ef08fbfa3d375a4f
Roby Firnando Yusuf <0x3a3a3a@gmail.com>
2023-05-28 23:11:53 +0700

    fix: model user

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: {
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
         type: String,
         required: true,
     },
